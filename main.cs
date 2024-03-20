ca459af4f4837aca978b4d44149fc4f004c09eee
s24816 <s24816@pjwstk.edu.pl>
2024-03-20 20:32:12 +0100

    max-of-numbers

--- main.cs
+++ main.cs
@@ -20,5 +20,18 @@ namespace MyApp
 
             return (double)sum / numbers.length;
         } 
+        public static int FindMaxValue(int[] numbers)
+    {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+
+        return max;
+    }
     }
 }
