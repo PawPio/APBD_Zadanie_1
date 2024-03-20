34a30d860ad500eef287aeee5ae5cdc5523d934c
s24816 <s24816@pjwstk.edu.pl>
2024-03-20 20:25:27 +0100

    commit_to_branch

--- main.cs
+++ main.cs
@@ -6,7 +6,19 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("mod_No_1");
+            int[] numbers = {5, 10, 15, 20, 25}
+            double average = CalculateAverage(numbers);
+            Console.WriteLine("average is equal to: " + average);
         }
+        public static double CalculateAverage(int[] numbers)
+        {
+            int sum = 0;
+            foreach(int num in numbers)
+            {
+                sum += sum;
+            }
+
+            return (double)sum / numbers.length;
+        } 
     }
 }
